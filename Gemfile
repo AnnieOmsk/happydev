a63748e86a318629362f3e4d788b575edfe649ea
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-08-23 17:34:37 +0700

    init

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'mysql2'
+gem 'haml-rails'
+gem 'json'
+gem 'libv8', '~> 3.11.8'
+gem 'therubyracer', '0.11.0beta5'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem "twitter-bootstrap-rails"
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happydev::Application.config.secret_token = 'acf88b82e617cd81b63c1630968bad83daccee9ef17f0c1f13f7da0a8cae152734c422ef5fd578ceb1da9d7dc7fbfc30baa633261a37527928ce9d3049284f50'
