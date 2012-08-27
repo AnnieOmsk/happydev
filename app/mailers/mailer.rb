1d6fbe6996d7157954f49ea8512c90b53da6045f
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-08-27 21:56:20 +0700

    use from in mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class Mailer < Devise::Mailer
   # helper :application # gives access to all helpers defined within `application_helper`.
-  default :from => "org@happydev.ru"
+  default :from => "HappyDev org@happydev.ru"
 
   # Send notification on mail user
   def send_notification(*content)
