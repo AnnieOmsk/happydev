6f24e3dc7156f70127f272a0e756b33a4381b4cc
Nikita Levchuk <nikitalevchuk@gmail.com>
2012-08-29 12:23:46 +0700

    added start_at and end_at to events views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class Event < ActiveRecord::Base
-  attr_accessible :name, :price, :master
+  attr_accessible :name, :price, :master, :start_at, :end_at, :place
   has_many :invoice_events
   has_many :invoices, :through => :invoice_events
 
