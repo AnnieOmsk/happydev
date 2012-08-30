01511ac9a5313644ff2e611d3d5a1964d765cc38
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-08-30 10:05:44 +0700

    add staging server capistrano configuration

---
+++ config/deploy/staging.rb
@@ -0,0 +1,13 @@
+set :rails_env,   "staging"
+ssh_options[:port] = 21722
+
+set :host,        'office.7bits.it'
+set :user,        'happydev'
+set :application, "happydev"
+
+role :app,        host  
+role :web,        host
+role :db,         host, :primary => true
+
+set :branch,    'develop'
+set :deploy_to, '$HOME/www/happydev'
