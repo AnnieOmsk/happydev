c089409bb3335df6d7607ed4db4af749f6f16a31
Ivan Nemytchenko <nemytchenko@gmail.com>
2012-09-04 21:17:50 +0700

    redirection test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-  def after_sign_in_path_for(resource)
-    user_profile_path
-    # pay_path
+  def after_sign_in_path_for(user)
+    if user.invoice.payment
+      root_path
+    else
+      pay_path
+    end
   end
 end
