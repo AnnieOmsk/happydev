e1d9f3305f25522c6507d61c72af11fab38840f8
Ivan Nemytchenko <nemytchenko@gmail.com>
2012-09-04 18:10:54 +0700

    hotfix for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def after_sign_in_path_for(resource)
-    # user_profile_path
-    pay_path
+    user_profile_path
+    # pay_path
   end
 end
