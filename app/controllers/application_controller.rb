fa39283d7aac8746720ddf6f6e3595f315e42b38
Ivan Nemytchenko <nemytchenko@gmail.com>
2012-09-05 22:57:13 +0700

    correct redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,13 @@ class ApplicationController < ActionController::Base
 
   private
   def after_sign_in_path_for(user)
-    if user.paid?
-      root_path
-    else
-      pay_path
-    end
-    # if user.invoice && user.invoice.payments.any?
+    root_path
+
+    # if user.paid?
     #   root_path
     # else
     #   pay_path
     # end
+
   end
 end
