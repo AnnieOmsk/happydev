385e3aac75beba74739b1ad12e73897b6938132a
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-06 12:14:38 +0700

    split user name and use first and last name in mailchimp

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   validates :professional, :presence => true
   validates :email, :presence => true, :uniqueness => true
 
+  before_create :split_name
   after_create :subscribe_to_mailchimp, :deliver_notification
 
   def paid?
@@ -21,11 +22,17 @@ class User < ActiveRecord::Base
 
   def subscribe_to_mailchimp
     @mailchimp = Mailchimp.new
-    #TODO send names to mailchimp too!!
-    @mailchimp.subscribe_to_list(email)
+    @mailchimp.subscribe_to_list(email, first_name, last_name)
   end
 
   def deliver_notification
     Mailer.send_notification(email).deliver!
+  end
+  
+  # split user name to first_name and last_name
+  # if user enter only one first_name, last_name will be nil
+  def split_name
+    array = name.split
+    self.first_name, self.last_name = array
   end
 end
