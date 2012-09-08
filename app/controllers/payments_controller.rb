27dce1d70182b43883fd58c257399d9c64554f3b
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-08 14:32:08 +0700

    payments controller change

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,7 @@ class PaymentsController < ApplicationController
   before_filter :create_notification, :only => [:success, :fail]
 
 
-  # Робокасса вызывает этот метод после успешной оплаты, перед тем, как вызвать success url.
+  # Робокасса вызывает этот метод после успешной оплаты, перед тем, как отправить юзера на success url.
   # Запрос производится после получения робокассой денег.
   # Запрос верен, если:
   # 1) Контрольная сумма верна
@@ -15,22 +15,34 @@ class PaymentsController < ApplicationController
   # Если всё окей, робокассе отправляется успешный ответ.
   def result
     @notification = Robokassa::Notification.new(request.raw_post, :secret => APP_CONFIG['robokassa']['secret2'])
+
     if @notification.acknowledge
-      render :text => @notification.success_response
+      @invoice = Invoice.find_by_code(@notification.item_id)
+
+      if @invoice
+        @invoice.payments.create(:amount => @notification.gross.to_i)
+        @invoice.mark_invoice_events_paid
+        # @invoice.mark_freeze!
+
+        if @invoice.user
+          Mailer.send_success_payment_notification(@invoice.user.email, @invoice).deliver!
+        else
+          logger.error "> Not found user for invoice: #{@notification.item_id}"
+          head :bad_request
+        end
+      else
+        logger.error "> Bad invoice: #{@notification.item_id}"
+        head :bad_request
+      end
     else
       head :bad_request
     end
   end
 
   # В случае успешного платежа покупатель переходит по этому адресу
-  # Если контрольная сумма верна и заказ ещё не помечен как оплаченный, то
-  # обновляем payment в базе, ставя ему paid = true
   def success
-    @invoice = current_user.invoice
-    if @notification.acknowledge && !@invoice.all_invoice_events_paid?
-      payment = @invoice.payments.create(:amount => @notification.gross.to_i)
-      @invoice.mark_invoice_events_paid
-      Mailer.send_success_payment_notification(current_user.email, @invoice).deliver!
+    if @notification.acknowledge
+      @invoice = current_user.invoice
       flash[:notice] = 'Оплата прошла успешно.'
       redirect_to invoice_path
     else
