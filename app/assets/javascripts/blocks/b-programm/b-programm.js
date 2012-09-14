71234057256118c4aa3ca19e2e17d7f914b87a55
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-14 13:55:28 +0700

    move js function to dom ready

--- app/assets/javascripts/blocks/b-programm/b-programm.js
+++ app/assets/javascripts/blocks/b-programm/b-programm.js
@@ -1,47 +1,49 @@
-$('.b-programm__column:last-child').addClass('b-programm__column-last');
+$(document).ready(function() {
+  $('.b-programm__column:last-child').addClass('b-programm__column-last');
 
-$(function() {
+  $(function() {
 
-    descLink = $('.js-b-programm__content');
-    descObj = $('.js-b-programm__desc');
+      descLink = $('.js-b-programm__content');
+      descObj = $('.js-b-programm__desc');
 
-    descLink.closeSelected = function() {
-        descObj.parent().filter('.selected').each(descLink.unSelected);
+      descLink.closeSelected = function() {
+          descObj.parent().filter('.selected').each(descLink.unSelected);
 
-        $(this).find(descObj).slideUp(100);
+          $(this).find(descObj).slideUp(100);
 
-        $(document).bind("vclick", descLink.unSelected);
-    }
+          $(document).bind("vclick", descLink.unSelected);
+      }
 
-    descLink.unSelected = function() {
-        descLink.removeClass('selected').find(descObj).slideUp(100);
-    }
+      descLink.unSelected = function() {
+          descLink.removeClass('selected').find(descObj).slideUp(100);
+      }
 
-    if(!$(window).hover) {
-        descLink.bind('vclick', function() {
-            if (!$(this).hasClass('selected')) {
-                $(this)
-                    .each(descLink.closeSelected)
-                    .addClass('selected').find(descObj).slideDown(200);
-                return false;
-            }
-        });
-    }
+      if(!$(window).hover) {
+          descLink.bind('vclick', function() {
+              if (!$(this).hasClass('selected')) {
+                  $(this)
+                      .each(descLink.closeSelected)
+                      .addClass('selected').find(descObj).slideDown(200);
+                  return false;
+              }
+          });
+      }
 
-    descLink.hover(function() {
+      descLink.hover(function() {
 
-        var elem = $(this);
+          var elem = $(this);
 
-        intervalElem = setTimeout(
+          intervalElem = setTimeout(
 
-            function() {
-                elem.addClass('selected').find(descObj).slideDown(200);
-            }, 800);
+              function() {
+                  elem.addClass('selected').find(descObj).slideDown(200);
+              }, 800);
 
-        }, function() {
-            clearInterval(intervalElem);
-            descLink.unSelected();
-        }
-    )
+          }, function() {
+              clearInterval(intervalElem);
+              descLink.unSelected();
+          }
+      )
 
+  });
 });
