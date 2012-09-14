ba0652e79ea33207f97c04285adc7d2ff5c18870
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-14 18:38:40 +0700

    strange rails bug monkeypatched

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  # hack for strange rails 3.2 production-only bug:
+  ActionView::Base.field_error_proc = Proc.new{ |html_tag, instance| "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe }
 
   def profile
     if current_user
