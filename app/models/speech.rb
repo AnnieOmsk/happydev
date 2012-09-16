2dae7aa214b98634c7ec3cf5d32e443c3e80a64d
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-17 00:19:58 +0700

    add company names to speakers in program and remove it from preview

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -39,6 +39,10 @@ class Speech < ActiveRecord::Base
   end
 
   def all_speakers_names
-    speakers.map {|s| "#{s.full_name} (s.company.name)" if s}.join(', ')
+    speakers.map {|s| s.full_name if s}.join(', ')
+  end
+  
+  def all_speakers_names_with_companies
+    speakers.map {|s| "#{s.full_name} (#{s.company.name})" if s}.join(', ')
   end
 end
