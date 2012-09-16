4fede5c460efec9fa6d7820375f89ec95e6098c4
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-16 23:23:04 +0700

    some scroll.js changes

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,16 +1,10 @@
-$(window).load(function() {
-  // anchor = window.location.hash;
-  // if(anchor != '') {
-  //   element = $('a[href="' + anchor + '"]');
-  //   scrollWithAnimation(element.attr('href'), 50, 1000);
-  // }
-  
+$(document).ready(function() {
   $('.b-head__nav .b-head__link').bind('click', function(event){
       event.preventDefault();
       scrollWithAnimation($(this).attr('href'), 50, 1000);
   });
-
-  function scrollWithAnimation(anchor, offset, delay) {
-    $('html, body').animate({scrollTop: $(anchor).offset().top - offset}, delay);
-  };
 });
+
+function scrollWithAnimation(anchor, offset, delay) {
+  $('html, body').animate({scrollTop: $(anchor).offset().top - offset}, delay);
+};
