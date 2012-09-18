2482cb1e17e2cc32ffe58077255ada3b077d3253
Nikita Levchuk <nikitalevchuk@gmail.com>
2012-09-18 18:00:54 +0700

    added name links for filter speakers carousel

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,8 @@
+# encoding: utf-8
 class Section < ActiveRecord::Base
   has_many :speeches
   attr_accessible :hall, :name, :speech_ids, :description
 
   validates :name, :presence => true
+  scope :without_general, where("name not in (?)", "Общий")
 end
--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
 class Specialization < ActiveRecord::Base
   has_many :speeches
   attr_accessible :name
   
   validates :name, :presence => true
+
+  scope :without_general, where("name not in (?)", ["Общий", "Стартап-порка"])
 end
