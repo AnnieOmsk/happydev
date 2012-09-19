c05811660e4678cfc3ee3270c6b6f2a0223fea5a
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-19 16:00:19 +0700

    move home controller redirections to routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,4 @@ class HomeController < ApplicationController
     @speeches = Speech.joins([:speaker, :section])
     @sections = Section.first(3)
   end
-
-  def about
-    redirect_to "/"
-  end
-
-  def program
-    redirect_to "/"
-  end
 end
