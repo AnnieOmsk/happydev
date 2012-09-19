fcd3ff06284ee8c14c1e4e7327147ef8036d79a2
saschalion <saschalion@gmail.com>
2012-09-19 16:14:27 +0700

    Tikets have been styled

---
+++ app/assets/javascripts/blocks/b-tikets/b-tikets.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  $(function() {
+
+      descLink = $('.js-b-tikets__link');
+
+      descLink.closeSelected = function() {
+          descLink.parent().filter('.b-tikets__method_state_active').each(descLink.unSelected);
+      }
+
+      descLink.unSelected = function() {
+          descLink.parent().removeClass('b-tikets__method_state_active');
+      }
+
+      if(!$(window).hover) {
+          descLink.bind('vclick', function() {
+              if (!$(this).parent().hasClass('b-tikets__method_state_active')) {
+                  $(this)
+                      .each(descLink.closeSelected)
+                      .parent().addClass('b-tikets__method_state_active');
+                  return false;
+              }
+          });
+      }
+
+      descLink.hover(function() {
+
+          var elem = $(this);
+
+          intervalElem = setTimeout(
+
+              function() {
+                  descLink.parent().removeClass('b-tikets__method_state_active');
+                  elem.parent().addClass('b-tikets__method_state_active');
+              }, 800);
+
+          }
+      )
+
+  });
+});
