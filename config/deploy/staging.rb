d9c1b8449deec067f844ad51a9c9d37b79b8e2b9
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-20 11:56:32 +0700

    update deploy branch for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,5 +9,5 @@ role :app,        host
 role :web,        host
 role :db,         host, :primary => true
 
-set :branch,    'invoice_for_students'
+set :branch,    'design_2.0'
 set :deploy_to, '$HOME/www/happydev'
