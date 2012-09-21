f4dc56548c9fc072f5e789b268adde72c4467de4
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-21 16:24:30 +0700

    add user email in yandex metrika report

--- app/assets/javascripts/metrika_callbacks.js
+++ app/assets/javascripts/metrika_callbacks.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("a[data-metrika='true']").click(function() {
     var targetName = $(this).data('target');
-    yaCounter17140027.reachGoal(targetName);
+    yaCounter17140027.reachGoal(targetName, window.yaParams || {});
   });
 
   $("#js-robokassa-form form").submit(function() {
     var input = $(this).find("input[data-metrika='true']");
     var targetName = input.data('target');
-    yaCounter17140027.reachGoal(targetName);
+    yaCounter17140027.reachGoal(targetName, window.yaParams || {});
   });
 });
