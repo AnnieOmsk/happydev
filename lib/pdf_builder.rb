5d4d6d2e9c8ffedf06e5400c934f0e8cce2f6d61
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-24 16:18:08 +0700

    pdf generation

---
+++ lib/pdf_builder.rb
@@ -0,0 +1,32 @@
+class PdfBuilder < ActionController::Base
+  include ApplicationHelper
+
+  def create_program_for_user(user)
+    @speeches = Speech.joins([:speaker, :section])
+    @sections = Section.first(3)
+    @user = user
+
+    pdf = render_to_string options_for_template("file.pdf")
+
+    save_path = Rails.root.join("pdfs/#{@user.full_name}.pdf")    
+    File.open(save_path, 'wb') do |file|
+      file << pdf
+    end
+  end
+
+  private
+  def options_for_template(name)
+     {
+      :pdf => name,
+      :encoding => 'utf8',
+      :layout => false,
+      :template => "programs/show",
+      :dpi => '300',
+      :margin => {:top => '5mm',
+                  :bottom => '0mm',
+                  :left => '5mm',
+                  :right => '0mm'},
+      :page_size => 'A4'
+    }
+  end
+end
