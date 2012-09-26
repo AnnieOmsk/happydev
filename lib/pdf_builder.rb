f451a5934a028ff49329d81b4bac20f7487f577c
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-09-26 18:19:47 +0700

    task for creating badges for users

--- lib/pdf_builder.rb
+++ lib/pdf_builder.rb
@@ -7,7 +7,7 @@ class PdfBuilder < ActionController::Base
     @sections = Section.first(3)
     @user = user
 
-    pdf = render_to_string options_for_template("file.pdf")
+    pdf = render_to_string options_for_template("file.pdf", "programs/show")
 
     save_path = Rails.root.join("pdfs/#{@user.full_name}.pdf")    
     File.open(save_path, 'wb') do |file|
@@ -15,19 +15,36 @@ class PdfBuilder < ActionController::Base
     end
   end
 
+  def create_badge_for_user(user)
+    @user = user
+    pdf = render_to_string options_for_template("badge.pdf", "badges/badge")
+    
+    background_path = Rails.root.join("badges/tmp/background_#{@user.id}.pdf")    
+    File.open(background_path, 'wb') do |file|
+      file << pdf
+    end
+    
+    badge_path = Rails.root.join('badges', 'templates', 'guest.pdf')
+    pdf_path = Rails.root.join('badges', 'final', "#{@user.id}.pdf")
+    system "pdftk #{background_path} background #{badge_path} output #{pdf_path}"
+  end
+
   private
-  def options_for_template(name)
+  def options_for_template(name, template)
      {
       :pdf => name,
       :encoding => 'utf8',
       :layout => false,
-      :template => "programs/show",
+      :template => template,
       :dpi => '300',
-      :margin => {:top => '5mm',
+      :margin => {:top => '50mm',
                   :bottom => '0mm',
-                  :left => '5mm',
+                  :left => '0mm',
                   :right => '0mm'},
-      :page_size => 'A4'
+      # :page_size => 'A4',
+      :page_height => '110',
+      :page_width => '90',
+      :no_background => true
     }
   end
 end
