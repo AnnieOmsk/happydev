48530a7a7143c8e7e9998561fe50e918e4fb397c
Nikita Levchuk <nikitalevchuk@gmail.com>
2012-09-28 13:47:24 +0700

    closed registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Happydev::Application.routes.draw do
 
   devise_for :users, :skip => [:registrations], :controllers => {:registrations => "registrations"}, :path => "registration", :path_names => { :sign_up => "new", :sign_in => 'login', :sign_out => 'logout' } do
     get 'registration/new' => 'devise/registrations#new', :as => 'new_user_registration'
-    post 'registration' => 'devise/registrations#create', :as => 'user_registration'
+    # post 'registration' => 'devise/registrations#create', :as => 'user_registration'
     get 'registration/profile' => 'registrations#profile', :as => 'user_profile'
     # get '                 registration/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     # put 'registration' => 'devise/registrations#update', :as => 'user_registration'
@@ -17,9 +17,9 @@ Happydev::Application.routes.draw do
   resources :likes, :only => [:index, :create, :update, :show]
 
   resources :events
-  resource :invoice, :only => [:new, :create, :show] do
-    delete :detach_from_user
-    put :clearing
+  resource :invoice, :only => [:new, :show] do
+    # delete :detach_from_user
+    # put :clearing
   end
 
   match '/about/requisites' => 'home#requisites'
@@ -32,11 +32,11 @@ Happydev::Application.routes.draw do
   match '/invoices/new' => 'invoices#new', :as => :pay
   match '/speakers/:permalink' => 'speeches#show'
 
-  scope 'payment' do
-    match 'result'    => 'payments#result',    :as => :payment_result # to handle Robokassa push request
-    match 'success' => 'payments#success', :as => :payment_success # to handle Robokassa success redirect
-    match 'fail'    => 'payments#fail',    :as => :payment_fail # to handle Robokassa fail redirect
-  end  
+  # scope 'payment' do
+    # match 'result'    => 'payments#result',    :as => :payment_result # to handle Robokassa push request
+    # match 'success' => 'payments#success', :as => :payment_success # to handle Robokassa success redirect
+    # match 'fail'    => 'payments#fail',    :as => :payment_fail # to handle Robokassa fail redirect
+  # end  
 
   # redirections (compatibility with old site and routes):
   match '/programme' => redirect('/')
