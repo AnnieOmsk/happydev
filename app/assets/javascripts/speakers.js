a8a186b36280f5e53af7dd800f1fbbcac6ae63f3
Sharipov Ruslan <sharipov.ru@gmail.com>
2012-10-02 12:17:31 +0700

    kill likes

--- app/assets/javascripts/speakers.js
+++ app/assets/javascripts/speakers.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  function get_like_status(type) {
-    var url = '/likes/' + window.location.pathname.split('/')[2];
-    $.ajax({
-      url: url,
-      type: type,
-      dataType: 'json',
-      success: function(data, textStatus, jqXHR) {
-        var star;
-        if (data.status && data.status == true) {
-          star = "★";
-        } else {
-          star = "☆";
-        }
-        $('#hd-like-star').html(star);
-      }
-    });
-  }
-
-  get_like_status("GET");
-
-  $('#hd-like-star').click(function(){
-    get_like_status("PUT");
-  });
-});
+// $(document).ready(function(){
+//   function get_like_status(type) {
+//     var url = '/likes/' + window.location.pathname.split('/')[2];
+//     $.ajax({
+//       url: url,
+//       type: type,
+//       dataType: 'json',
+//       success: function(data, textStatus, jqXHR) {
+//         var star;
+//         if (data.status && data.status == true) {
+//           star = "★";
+//         } else {
+//           star = "☆";
+//         }
+//         $('#hd-like-star').html(star);
+//       }
+//     });
+//   }
+// 
+//   get_like_status("GET");
+// 
+//   $('#hd-like-star').click(function(){
+//     get_like_status("PUT");
+//   });
+// });
