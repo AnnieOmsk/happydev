12112e534c4f7e0c291e35f4c858805fa4728c72
Sharipov Ruslan <sharipov.ru@gmail.com>
2013-06-13 17:06:55 +0700

    fixed issues with sidekiq processing

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,14 @@
 redis = YAML.load_file("#{Rails.root.to_s}/config/config.yml")[Rails.env]['redis']
 
 # PASSWORD_REQUIRED = %w(production staging)
+PASSWORD_REQUIRED = []
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "redis://#{redis['host']}:#{redis['port']}/#{redis['db']}", :namespace => Rails.env }
+  config.redis = { :url => "redis://#{':' + redis['password'] if PASSWORD_REQUIRED.include?(Rails.env)}@#{redis['host']}:#{redis['port']}/#{redis['db']}", :namespace => Rails.env }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis://#{redis['host']}:#{redis['port']}/#{redis['db']}", :namespace => Rails.env, :size => 1 }
+  config.redis = { :url => "redis://#{':' + redis['password'] if PASSWORD_REQUIRED.include?(Rails.env)}@#{redis['host']}:#{redis['port']}/#{redis['db']}", :namespace => Rails.env, :size => 1 }
 end
 
 Sidekiq::Mailer.excluded_environments = [:test]
